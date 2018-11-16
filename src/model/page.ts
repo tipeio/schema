1f39847729a4fda06058f2d9a378dc5325819875
Scott Moss <willscottmoss@gmail.com>
2018-11-16 13:30:02 -0800

    fix(models): remove system
    remove system models that need to be computed
    remove system fields that need to be computed

--- src/model/page.ts
+++ src/model/page.ts
@@ -1,20 +1,4 @@
 import { Model } from './model'
-import { IUserSchema, IFields } from '../types'
-import { fields, models, types } from '../utils'
 export class Page extends Model {
   public readonly modelType = 'Page'
-
-  protected transformSchemaFields(userFields: IUserSchema): IFields {
-    // add meta fields
-    const metaField = {
-      [fields.meta]: {
-        type: types.shape,
-        ref: models.meta
-      }
-    }
-    // overwrite field with same name if there
-    const finalFields = {...userFields, ...metaField}
-
-    return super.transformSchemaFields(finalFields)
-  }
 }
