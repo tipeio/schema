96bf9e21f85c9acfaf2a905587bafca969b3d790
Scott Moss <willscottmoss@gmail.com>
2019-04-16 14:22:13 -0700

    fix(types): exports
    fix exports for fieldTypes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './shape'
 export * from './page'
 export * from './schema'
-export * from './utils/systemModels'
 export * from './utils'
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 export * from './error'
 export * from './constants'
+export * from './systemModels'
+export { fieldTypes } from './fieldTypes'
